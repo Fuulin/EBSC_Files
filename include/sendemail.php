dd03466a46475c3c1d216beabc9743204958c243
Linh Ha <linh.ha1409@gmail.com>
2018-09-21 11:36:56 +0800

    finished dev

--- include/sendemail.php
+++ include/sendemail.php
@@ -8,8 +8,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'username@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'norazlin@ebsc.my', // Your Email Address
+				'name' => 'Norazlin' // Your Name
 			);
 
 // Form Processing Messages
